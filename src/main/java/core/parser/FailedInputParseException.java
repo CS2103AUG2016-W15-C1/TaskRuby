f1fd1682a9efa85ca57ba3e9456ba5d9d9c88316
Alex <dev@xrng.net>
2016-10-16 22:40:11 +0800

    Add simple parser

---
+++ src/main/java/core/parser/FailedInputParseException.java
@@ -0,0 +1,13 @@
+package core.parser;
+
+public class FailedInputParseException extends Exception {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -4300334938845829613L;
+    public FailedInputParseException(String message) {
+        super(message);
+    }
+
+}
