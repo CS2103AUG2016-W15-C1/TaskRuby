6e8c6c4682dc1cafe55770afc692edbe63476962
Alex <dev@xrng.net>
2016-10-18 16:25:37 +0800

    add tests for delete comman

---
+++ src/test/java/DeleteCommandTest.java
@@ -0,0 +1,55 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import core.CommandException;
+import core.DatabaseStorage;
+import core.DeleteCommand;
+import core.StorageBackend;
+import core.StorageException;
+import models.Task;
+
+public class DeleteCommandTest {
+    
+    private StorageBackend storage;
+    private DeleteCommand deleteCommand;
+
+    @Before
+    public void setUp() throws Exception {
+        storage = new DatabaseStorage("DeleteCommandTest.db");
+        storage.initializeStorage();
+        deleteCommand = new DeleteCommand(storage);
+        
+        storage.addTask(new Task("test1"));
+        storage.addTask(new Task("test2"));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        storage.deleteStorage();
+    }
+
+    @Test
+    public void test() {
+        String[] cmd = {"2"};
+        String[] cmd2 = {"1"};
+        try {
+            assertEquals("that the size is 2 to begin with", 
+                    storage.getTasks().size(), 2);
+            deleteCommand.execute(cmd);
+            assertEquals("that the size is 1 after deleting 1", 
+                    storage.getTasks().size(), 1);
+            deleteCommand.execute(cmd2);
+            assertEquals("that the size is 0 after deleting 1 more", 
+                    storage.getTasks().size(), 0);
+        } catch (CommandException | StorageException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+        
+    }
+
+}
