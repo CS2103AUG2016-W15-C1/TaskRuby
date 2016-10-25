4ca8c376a032bd9abb76b45b8d23c5877e6797b0
Alex <dev@xrng.net>
2016-10-26 05:15:01 +0800

    add extra task attributes

--- src/main/java/models/Task.java
+++ src/main/java/models/Task.java
@@ -1,6 +1,10 @@
 package models;
 
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -8,6 +12,9 @@ import javafx.beans.property.StringProperty;
 public class Task {
     private IntegerProperty taskIdentifier;
     private final StringProperty taskShortName;
+    private ObjectProperty<ZonedDateTime> taskStartTime;
+    private ObjectProperty<ZonedDateTime> taskDeadline;
+    private StringProperty taskPriority; // this should be an enum
     
     public Task(String taskName) {
         this.taskShortName = new SimpleStringProperty(taskName);
@@ -41,5 +48,29 @@ public class Task {
     
     public void setTaskIdentifier(int id) {
         this.taskIdentifier = new SimpleIntegerProperty(id);
+    }
+    
+    public StringProperty taskPriority() {
+        return taskPriority;
+    }
+    
+    public void setTaskPriority(String p) {
+        this.taskPriority = new SimpleStringProperty(p);
+    }
+    
+    public void setTaskDeadline(ZonedDateTime time) {
+        this.taskDeadline.set(time);
+    }
+    
+    public String getTaskDeadline() {
+        return this.taskDeadline.get().toString();
+    }
+    
+    public void setTaskStartTime(ZonedDateTime time) {
+        this.taskStartTime.set(time);
+    }
+    
+    public String getTaskStartTime() {
+        return this.taskStartTime.get().toString();
     }
 }
